400c5f4b871c115171ef0220471958b1dc0603f6
MohsinBashir-prog <1happy.journey123@gmail.com>
2026-08-19 22:00:57 +0500

    Add new product to the products array

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const products=[
 {id:1,name:"Mint Party Frock",category:"Clothes",price:3000,oldPrice:4000,age:"5–8 years",icon:"👗",image:"WhatsApp Image 2026-08-18 at 12.51.28 AM.jpeg"},
   {id:2,name:"Brown Baby Shoes",category:"Shoes",price:999,age:"Newborn–1 year",icon:"👟",image:"WhatsApp Image 2026-08-14 at 10.39.45 PM.jpeg"},
 {id:3,name:"Brown Baby Shoes",category:"Shoes",price:999,age:"Newborn–1 year",icon:"👟",image:"WhatsApp Image 2026-08-14 at 10.39.47 PM.jpeg"},
+{id:7,name:"Brown Baby Shoes",category:"Shoes",price:999,age:"Newborn–1 year",icon:"👟",image:"WhatsApp Image 2026-08-14 at 10.39.48 PM.jpeg"},
   
   {id:4,name:"Product 4",category:"Toys",icon:"🧸"},
   {id:5,name:"Product 5",category:"Accessories",icon:"🍼"},
